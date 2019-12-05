90ec7bdbbb0f8eea507a93246fca7080922d7b21
jooin95 <jooin95@naver.com>
2019-12-06 00:18:52 +0900

    api

--- Highway to hell/visualization/views.py
+++ Highway to hell/visualization/views.py
@@ -1,26 +1,10 @@
+from json import encoder
 from django.shortcuts import render
 from .models import TrendWithSales, TrendWithItemFreq, keyword_insert, topic_class,practice, publish_onion
 from .ldafile import lda, correlation
 from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseNotFound
-from django.db.models import Q, F, Value
-from django.db.models.functions import Concat
-from django.db import connection
-from datetime import datetime, timedelta
-from django.db.models import Avg, Sum, Count
-import time
-import json
-import pandas as pd
-from pymongo import MongoClient
-import pandas as pd
-import numpy as np
-from functools import reduce
-import operator
-#from konlpy.tag import Kkma # 형태소분석기를 만들기 위해서 설정함.
-from collections import Counter # Counter생성함.
-# from konlpy.tag import Hannanum # 형태소분석기를 만들기 위해서 설정함.
-import operator # dictionary를 정렬하기 위해서 구현함.
-from collections import defaultdict
-
+import subprocess
+from urllib import parse
 from django.views.decorators.csrf import csrf_exempt
 
 from django.core.serializers.json import DjangoJSONEncoder
@@ -38,32 +22,31 @@ def test_send(request):
     startDate = request.POST['StartDate']
     start_point = request.POST['start_point']
     finish_point = request.POST['finish_point']
+    test1 = parse.quote_plus(start_point)
+    test2 = parse.quote_plus(finish_point)
+    cmd = ['curl',
+           'https://naveropenapi.apigw.ntruss.com/map-place/v1/search?query=' +
+           test1 + '&coordinate=127.1054328,37.3595963',
+           '-H', 'X-NCP-APIGW-API-KEY-ID:x2i0xjwran', '-H',
+           'X-NCP-APIGW-API-KEY:ced9h4Hk4cUKJmCqa2QcUV3Ows7I0byrLEogtWdr', '-v'
+          ]
+    f = subprocess.Popen(cmd, stdout=subprocess.PIPE, encoding="utf-8").stdout
+    data1 = f.read().strip()
+    f.close()
+    cmd = ['curl',
+           'https://naveropenapi.apigw.ntruss.com/map-place/v1/search?query=' +
+           test2 + '&coordinate=127.1054328,37.3595963',
+           '-H', 'X-NCP-APIGW-API-KEY-ID:x2i0xjwran', '-H',
+           'X-NCP-APIGW-API-KEY:ced9h4Hk4cUKJmCqa2QcUV3Ows7I0byrLEogtWdr', '-v'
+           ]
+    f = subprocess.Popen(cmd, stdout=subprocess.PIPE, encoding="utf-8").stdout
+    data2 = f.read().strip()
+
     sel_list = {
         'startDate': startDate,
         'start_point': start_point,
         'finish_point': finish_point,
     }
-    print(startDate)
-    print(start_point)
-    print(finish_point)
-    # cursor = connection.cursor()
-    # print(startDate)
-    # query_string = "SELECT GetDT, ElePower, Ampere, Voltage, CastSpeed," \
-    #                "Frequency FROM TP_MDF_GETDATA " \
-    #                "where GetDT >=%s LIMIT 100;"
-    # cursor.execute(query_string, startDate)
-    # rows = cursor.fetchall()
-    # data_list = []
-    # for row in rows:
-    #     # date = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').date()
-    #     dic = {
-    #         'GetTime': row[0],
-    #         'ElePower': row[1],
-    #         'Ampere': row[2],
-    #         'Voltage': row[3],
-    #         'CastSpeed': row[4],
-    #         'Frequency': row[5]
-    #     }
-    #     data_list.append(dic)
-    # data_list = json.dumps(data_list, cls=DjangoJSONEncoder)
-    return JsonResponse({"Sel_list": sel_list})
+    print(data1)
+    print(data2)
+    return JsonResponse({"data1": data1, "data2": data2, "startDate": startDate})
