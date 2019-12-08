7640fdadf953f664589ab4d7942418f25bd2a6ff
강승우 <portt1997@gmail.com>
2019-12-09 06:09:25 +0900

    Update InsertionOfPlaces.py

--- 강승우/InsertionOfPlaces.py
+++ 강승우/InsertionOfPlaces.py
@@ -44,8 +44,7 @@ try:
 			data1 = f.read().strip()
 			f.close()
 			data1 = json.dumps(data1, cls=DjangoJSONEncoder)
-			print(data1)
-			insertion[k] += data1['places'][0]['x'] + "," + data1['places'][0]['y'] + ");"
+			insertion[k] += data1[0]['places'][0]['x'] + "," + data1[0]['places'][0]['y'] + ");"
 			i += 1
 		k += 1
 	
