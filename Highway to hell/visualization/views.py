bf37012295a595c1ed7d27260492c55618f36bea
강승우 <portt1997@gmail.com>
2019-12-09 22:57:45 +0900

    asdsd

--- Highway to hell/visualization/views.py
+++ Highway to hell/visualization/views.py
@@ -12,7 +12,6 @@ import time
 from django.db import connection
 import pymysql
 from traceback import format_exc
-from collections import OrderedDict
 
 myNaverKey1 = "x2i0xjwran"
 myNaverKey2 = "ced9h4Hk4cUKJmCqa2QcUV3Ows7I0byrLEogtWdr"
@@ -108,9 +107,7 @@ def test_analysis(request):
     data1 = request.POST["data1"]
     data2 = request.POST["data2"]
     data3 = request.POST["data3"]
-    gui = request.POST["gui"]
-
-    distance_time = request.POST["distance_time"]
+	
     type = request.POST.get('guide1','')
     duration = request.POST.get('guide2','')
     print(type)
@@ -163,89 +160,5 @@ def test_analysis(request):
     print(select)
     final = select
     final = json.dumps(final, cls=DjangoJSONEncoder, ensure_ascii=False)
-    expected_time = get_expectedTime(gui, distance_time, startDate)
     return JsonResponse({"select": select})
 
-def get_expectedTime(guide, distance_time, startDate):
-	
-	expected_time = 1
-	return expected_time
-	
-
-def get_token(token):
-	db = pymysql.connect("localhost","root","1234","highwaytohell",charset="utf8")
-
-	try:
-		cursor = db.cursor()
-
-		sql = "select * from place"
-		cursor.execute(sql)
-		places = cursor.fetchall()
-		
-		cmd = ['curl',
-		   'https://naveropenapi.apigw.ntruss.com/map-place/v1/search?query=' +
-		   parse.quote_plus(token) + '&coordinate=127.1026513,37.2654939',
-		   '-H', 'X-NCP-APIGW-API-KEY-ID:' + myNaverKey1, '-H',
-		   'X-NCP-APIGW-API-KEY:' + myNaverKey2, '-v'
-		   ]
-		f = subprocess.Popen(cmd, stdout=subprocess.PIPE, encoding="utf-8").stdout
-		data1 = f.read().strip()
-		f.close()
-		data = json.loads(data1)
-		lng = float(data['places'][0]['x'])
-		lat = float(data['places'][0]['y'])
-		
-		CurID = ""
-		newID = ""
-		CurCom = 0.0
-		newCom = 0.0
-		for row in places:
-			if CurID == "" :
-				CurID = row[0]
-				CurCom = abs((lng + lat - row[1] - row[2])**2)
-			else:
-				newID = row[0]
-				newCom = abs((lng + lat - row[1] - row[2])**2)
-				if newCom < CurCom :
-					CurCom = newCom
-					CurID = newID
-		print(CurCom)
-		way = CurID.split('h')
-		sql = "select * from highways where ID = '" + way[0] + "';"
-		cursor.execute(sql)
-		highway = cursor.fetchall()
-		maintoken = highway[0][2+int(way[1])]
-		
-		sql = "select * from place where ID ='"+CurID+"'"
-		cursor.execute(sql)
-		places = cursor.fetchall()
-		CurLng = places[0][1]
-		CurLat = places[0][2]
-		
-		sql = "select * from place where lng = "+ format(CurLng,'12.8f') + " and lat = " + format(CurLat,'12.8f') + ";" 
-		cursor.execute(sql)
-		places = cursor.fetchall()
-		
-		ways = []
-		tokens = []
-		
-		for row in places:
-			hightoken = (row[0].split('h'))[0]
-			ways.append(hightoken)
-			tokens.append("h"+(row[0].split('h'))[1])
-		
-		print (maintoken)
-		print (tokens)
-		print (ways)
-		rec = OrderedDict()
-		rec["tokens"] = tokens
-		rec["ways"] = ways
-		rt = json.dumps(rec, ensure_ascii=False, indent="\t")
-		dat = json.loads(rt)
-		print (dat)
-	except IndexError:
-		print(format_exc())
-
-	finally:
-		db.close()
-	return dat
